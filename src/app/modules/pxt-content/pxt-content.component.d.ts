34165506b63a719b07cc581a611c91b0a21de129
PXTUDI\avilandersc <avilandersc@peixoto.com.br>
2018-08-28 16:07:28 -0300

    teste 6

--- src/app/modules/pxt-content/pxt-content.component.d.ts
+++
@@ -1,6 +0,0 @@
-import { OnInit, OnDestroy } from '@angular/core';
-export declare class PxtContentComponent implements OnInit, OnDestroy {
-    ngOnDestroy(): void;
-    ngOnInit(): void;
-    constructor();
-}
