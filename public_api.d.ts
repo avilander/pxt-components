6b8a573b682e50be2531cc23ef446b98568f5dd1
PXTUDI\avilandersc <avilandersc@peixoto.com.br>
2018-08-28 16:14:57 -0300

    teste9

--- public_api.d.ts
+++ public_api.d.ts
@@ -1,1 +1,2 @@
 export * from './src/app/modules/pxt-app/pxt-app.module';
+export * from './src/app/modules/pxt-app/pxt-app.component';
--- pxt-shared-components.d.ts
+++ pxt-shared-components.d.ts
@@ -2,6 +2,5 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public_api';
-export { PxtContentBody as ɵc } from './src/app/directives/pxt-content-body';
+export { PxtContentBody as ɵb } from './src/app/directives/pxt-content-body';
 export { MaterialAngularModule as ɵa } from './src/app/modules/material-angular/material-angular.module';
-export { PxtAppComponent as ɵb } from './src/app/modules/pxt-app/pxt-app.component';
