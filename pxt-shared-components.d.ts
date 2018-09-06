5b1d721fe19a08eb17d148e6601f3a275e8ccb10
PXTUDI\avilandersc <avilandersc@peixoto.com.br>
2018-09-06 16:38:05 -0300

    teste9

--- pxt-shared-components.d.ts
+++ pxt-shared-components.d.ts
@@ -2,6 +2,5 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public_api';
-export { PxtContentBody as ɵb } from './src/app/directives/pxt-content-body';
-export { DefaultModule as ɵa } from './src/app/modules/default.module';
-export { RequestBaseService as ɵc } from './src/app/services/pxt-http/request-base.service';
+export { PxtContentBody as ɵa } from './src/app/directives/pxt-content-body';
+export { RequestBaseService as ɵb } from './src/app/services/pxt-http/request-base.service';
