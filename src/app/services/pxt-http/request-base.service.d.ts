b5b5bbf7fbf1f7a88a784969c6fb7ee28dc2c725
PXTUDI\avilandersc <avilandersc@peixoto.com.br>
2018-09-26 11:36:20 -0300

    correção crud automático

--- src/app/services/pxt-http/request-base.service.d.ts
+++ src/app/services/pxt-http/request-base.service.d.ts
@@ -1,12 +1,16 @@
 import { PxtHttpService } from './pxt-http.service';
 import { HttpHelperService } from './http-helper-service';
+import { TokenService } from './token.service';
+import { HttpClient } from '@angular/common/http';
 export declare class RequestBaseService<T> {
     private httpService;
     private helper;
+    private tokenService;
+    _httpClient: HttpClient;
     model: T;
     urlService: string;
     urlServiceAuto: string;
-    constructor(httpService: PxtHttpService, helper: HttpHelperService);
+    constructor(httpService: PxtHttpService, helper: HttpHelperService, tokenService: TokenService, _httpClient: HttpClient);
     load(): any;
     save(model?: T): any;
     delete(id: any): any;
@@ -14,4 +18,7 @@ export declare class RequestBaseService<T> {
     doPost(path: string, model?: T): any;
     doPut(path: string, model?: T): any;
     doDelete(path: string, id: number): any;
+    uploadImage(path: any, params?: Map<any, any>): any;
+    setParamsFormdata(params: Map<any, any>): FormData;
+    private buildRequestParams(params);
 }
