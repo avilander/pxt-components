07ec468f9a252e36ebeb816027513c76237d0a69
PXTUDI\avilandersc <avilandersc@peixoto.com.br>
2018-11-06 11:20:26 -0200

    Correção 123

--- src/app/visible-in-roles.guard.d.ts
+++ src/app/visible-in-roles.guard.d.ts
@@ -3,11 +3,15 @@ import { Observable } from 'rxjs';
 import { HttpHelperService } from './services/pxt-http/http-helper-service';
 import { AuthorityService } from './services/authority.service';
 import { UserService } from './services/user.service';
+import { MatDialog } from '@angular/material';
 export declare class VisibleInRolesGuard implements CanActivate {
     private router;
     private httpHelper;
     private authorityService;
     private userService;
-    constructor(router: Router, httpHelper: HttpHelperService, authorityService: AuthorityService, userService: UserService);
+    private dialog;
+    private urlHelper;
+    constructor(router: Router, httpHelper: HttpHelperService, authorityService: AuthorityService, userService: UserService, dialog: MatDialog, urlHelper: HttpHelperService);
     canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean;
+    openDialog(erro: any): void;
 }
